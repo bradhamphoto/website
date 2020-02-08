668f485be551b4ce3ee1e9398c1b58716e7f71fd
kgrubb <keligrubb324@gmail.com>
2020-02-07 23:16:47 -0500

    initial commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'James Bradham',
+  shortName: 'James Bradham',
+  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Imed Adel',
+  twitter: 'Imed_Adel'
+}
---
+++ src/components/layout.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Layout = props => {
+  const { title, children } = props
+  const [toggleNav, setToggleNav] = React.useState(false)
+  return (
+    <div className={`site-wrapper ${toggleNav ? `site-head-open` : ``}`}>
+      <header className="site-head">
+        <div className="site-head-container">
+          <a
+            className="nav-burger"
+            href={`#`}
+            onClick={() => setToggleNav(!toggleNav)}
+          >
+            <div
+              className="hamburger hamburger--collapse"
+              aria-label="Menu"
+              role="button"
+              aria-controls="navigation"
+            >
+              <div className="hamburger-box">
+                <div className="hamburger-inner" />
+              </div>
+            </div>
+          </a>
+          <nav id="swup" class="site-head-left">
+            <ul className="nav" role="menu">
+              <li className="nav-home nav-current" role="menuitem">
+                <Link to={`/`}>Home</Link>
+              </li>
+              <li className="nav-contact" role="menuitem">
+                <Link to={`/contact`}>Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="site-head-center">
+            <Link className="site-head-logo" to={`/`}>
+              {title}
+            </Link>
+          </div>
+          <div className="site-head-right">
+          </div>
+        </div>
+      </header>
+      <main id="site-main" className="site-main">
+        <div id="swup" className="transition-fade">
+          {children}
+        </div>
+      </main>
+      <footer className="site-foot">
+        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
